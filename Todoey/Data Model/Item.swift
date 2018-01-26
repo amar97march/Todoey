7d2b07289b544914b31377b34ab4bbf481f75d53
Amar Singh <amar97march@gmail.com>
2018-01-26 09:56:53 +0530

    converted to MVC with shortening of code and fixed repetition of checkmarks

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Amar Singh on 26/01/18.
+//  Copyright © 2018 Amar Singh. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+    
+}
